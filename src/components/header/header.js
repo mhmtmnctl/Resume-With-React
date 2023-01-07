bdee59b2b4eedbc291fe991b1355d6d13d0731f1
Mehmet Emin Çatal <mhmtmnctl20@gmail.com>
2023-01-07 10:42:35 +0300

    resume-first-component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,12 +2,14 @@ import React from "react";
 import Avatar from "./avatar/avatar";
 import MobileMenu from "./mobile-menu/mobile-menu";
 import "./header.css";
+import UserName from "./user-name/userName";
 
 const Header = () => {
   return (
     <header className="header pull-left">
       <MobileMenu />
       <Avatar />
+      <UserName />
     </header>
   );
 };
---
+++ src/components/header/user-name/userName.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./userName.css";
+
+const UserName = () => {
+  return (
+    <div className="name">
+      <h1>Mehmet Emin ÇATAL</h1>
+      <span>Full Stack Java Developer</span>
+    </div>
+  );
+};
+
+export default UserName;
